895143bd4fd25ceb46b8b8f538b821dd4931e0b2
aahadrahi <aahadrahi786@gmail.com>
2023-05-15 20:01:36 +0530

    pass

--- hhc/settings.py
+++ hhc/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
+# adminuser;'HHC4U'
+# adminpass:'HHC'
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
